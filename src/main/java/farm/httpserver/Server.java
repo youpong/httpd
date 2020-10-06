fc84823ec0add0c02c088bbdd60dbace392acf3b
NAKAJIMA Yusaku <yupong7@gmail.com>
2020-10-06 20:16:25 +0900

    change opening message.

--- src/main/java/farm/httpserver/Server.java
+++ src/main/java/farm/httpserver/Server.java
@@ -41,7 +41,7 @@ public class Server {
 	}
 
 	private void printHostPort(ServerSocket svSock) {
-		System.out.println(
+		System.out.println("Server listen " +
 				svSock.getInetAddress().getHostName() + ":" + svSock.getLocalPort());
 	}
 }
