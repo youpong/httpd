d44111d50a88f0c921ef58d26ec59e9c1e26cbba
NAKAJIMA Yusaku <yupong7@gmail.com>
2020-10-07 19:56:50 +0900

    delete unused import.

--- src/test/java/farm/HttpRequestTest.java
+++ src/test/java/farm/HttpRequestTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 public class HttpRequestTest {
 
